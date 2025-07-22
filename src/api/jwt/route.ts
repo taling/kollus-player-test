6fbc61ea3122089242e04a4d59c235d41c0ae4cf
JD <jd@taling.me>
2025-07-22 12:39:33 +0900

    feat: add JWT API route template

---
+++ src/api/jwt/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+// 여기에 JWT 토큰 생성 API를 구현해주세요
+export async function GET() {}
+export async function POST(request: NextRequest) {}
